621549321dd10e5f28ca7b9f1eeae4d0983dc5cb
MDhanesh <mgdhanesh98@gmail.com>
2023-03-09 16:20:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,207 @@
+//starting
+let calendar = document.querySelector(".calendar");
+let clicked = null;
+// events store
+let events = localStorage.getItem("events")
+  ? JSON.parse(localStorage.getItem("events"))
+  : [];
+
+const newEventModal = document.getElementById("newEventModal");
+const deleteEventModal = document.getElementById("deleteEventModal");
+const backDrop = document.getElementById("modalBackDrop");
+const eventTitleInput = document.getElementById("eventTitleInput");
+//month names
+const month_names = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+//open modal
+function openModal(date) {
+  clicked = date;
+
+  const eventForDay = events.find((e) => e.date === clicked);
+
+  if (eventForDay) {
+    document.getElementById("eventText").innerText = eventForDay.title;
+    deleteEventModal.style.display = "block";
+  } else {
+    newEventModal.style.display = `block`;
+  }
+  backDrop.style.display = `block`;
+}
+//find leap year
+isLeapYear = (year) => {
+  return (
+    (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
+    (year % 100 === 0 && year % 400 === 0)
+  );
+};
+
+getFebDays = (year) => {
+  return isLeapYear(year) ? 29 : 28;
+};
+// calendar start
+generateCalendar = (month, year) => {
+  let calendar_days = calendar.querySelector(".calendar-days");
+  let calendar_header_year = calendar.querySelector("#year");
+
+  let days_of_month = [
+    31,
+    getFebDays(year),
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31,
+  ];
+
+  calendar_days.innerHTML = "";
+
+  let currDate = new Date();
+  const currday = currDate.getDate();
+  if (!month) month = currDate.getMonth();
+  if (!year) year = currDate.getFullYear();
+
+  let curr_month = `${month_names[month]}`;
+  month_picker.innerHTML = curr_month;
+  calendar_header_year.innerHTML = year;
+
+  // get first day of month
+
+  let first_day = new Date(year, month, 1);
+
+  for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+    let day = document.createElement("div");
+    const dayString = `${i - currday}/${month + 1}/${year}`;
+
+    if (i >= first_day.getDay()) {
+      const eventForDay = events.find((e) => e.date === dayString);
+      day.classList.add("calendar-day-hover");
+      day.innerHTML = i - first_day.getDay() + 1;
+      day.innerHTML += `<span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>`;
+      if (
+        i - first_day.getDay() + 1 === currDate.getDate() &&
+        year === currDate.getFullYear() &&
+        month === currDate.getMonth()
+      ) {
+        day.classList.add("curr-date");
+      }
+      ///
+      if (eventForDay) {
+        const eventDiv = document.createElement("div");
+        eventDiv.classList.add("event");
+        eventDiv.innerText = eventForDay.title;
+        day.appendChild(eventDiv);
+      }
+
+      day.addEventListener("click", () => openModal(dayString));
+      ///
+    }
+    calendar_days.appendChild(day);
+  }
+};
+
+let month_list = calendar.querySelector(".month-list");
+
+month_names.forEach((e, index) => {
+  let month = document.createElement("div");
+  month.innerHTML = `<div data-month="${index}">${e}</div>`;
+  month.querySelector("div").onclick = () => {
+    month_list.classList.remove("show");
+    curr_month.value = index;
+    generateCalendar(curr_month.value, curr_year.value);
+  };
+  month_list.appendChild(month);
+});
+
+let month_picker = calendar.querySelector("#month-picker");
+
+month_picker.onclick = () => {
+  month_list.classList.add("show");
+};
+
+let currDate = new Date();
+
+let curr_month = { value: currDate.getMonth() };
+let curr_year = { value: currDate.getFullYear() };
+//save function
+function saveEvent() {
+  if (eventTitleInput.value) {
+    eventTitleInput.classList.remove("error");
+    events.push({
+      date: clicked,
+      title: eventTitleInput.value,
+    });
+    localStorage.setItem("events", JSON.stringify(events));
+    closeModal();
+  } else {
+    eventTitleInput.classList.add("error");
+  }
+}
+//close function
+function closeModal() {
+  eventTitleInput.classList.remove("error");
+  newEventModal.style.display = "none";
+  deleteEventModal.style.display = "none";
+  backDrop.style.display = "none";
+  eventTitleInput.value = "";
+  clicked = null;
+  generateCalendar(curr_month.value, curr_year.value);
+}
+//delete function
+function deleteEvent() {
+  events = events.filter((e) => e.date !== clicked);
+  localStorage.setItem("events", JSON.stringify(events));
+  closeModal();
+}
+
+function initButtons() {
+  document.getElementById("saveButton").addEventListener("click", saveEvent);
+
+  document.getElementById("cancelButton").addEventListener("click", closeModal);
+
+  document
+    .getElementById("deleteButton")
+    .addEventListener("click", deleteEvent);
+
+  document.getElementById("closeButton").addEventListener("click", closeModal);
+}
+initButtons();
+
+generateCalendar(curr_month.value, curr_year.value);
+/// year button on change
+document.querySelector("#prev-year").onclick = () => {
+  --curr_year.value;
+  generateCalendar(curr_month.value, curr_year.value);
+};
+
+document.querySelector("#next-year").onclick = () => {
+  ++curr_year.value;
+  generateCalendar(curr_month.value, curr_year.value);
+};
+
+//dark and light mode
+let dark_mode_toggle = document.querySelector(".dark-mode-switch");
+
+dark_mode_toggle.onclick = () => {
+  document.querySelector("body").classList.toggle("light");
+  document.querySelector("body").classList.toggle("dark");
+};
